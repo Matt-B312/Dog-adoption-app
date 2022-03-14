b9e307f5cea6fba80b143b104c32630e1ad2130f
Matthew Lebreton <mlebreton84813@gmail.com>
2022-03-14 15:35:12 -0400

    mongooseflights files

---
+++ controllers/tickets.js
@@ -0,0 +1,19 @@
+
+const Ticket = require('../models/ticket');
+
+
+function newTicket(req, res) {
+    res.render('flights/newticket', {flightId: req.params.id})
+}
+
+
+async function addTicket(req, res) {
+    req.body.flight = req.params.id;
+    await Ticket.create(req.body);
+    res.redirect(`/flights/${req.params.id}`);
+}
+
+module.exports ={
+    add: addTicket,
+    new: newTicket
+}
